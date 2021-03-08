90174b5b5bd04ad3e6e8dd30708a183208e846fa
HeZhao_Peko <zh0208@163.com>
2021-03-09 05:25:41 +0800

    # problem 1

--- src/main/java/problem1/Problem1_AlgorithmAnalysis.java
+++ src/main/java/problem1/Problem1_AlgorithmAnalysis.java
@@ -2,12 +2,25 @@ package problem1;
 
 // (4 pt) Give Theta of the running time (as a function of n) of the following code snippets:
 // (a)
-// Your answer:
+// Your answer: Theta(n^3)
 // Justification:
+// For the outer loop, it is Theta(n).
+// And for the inner loop, it is (n-1) + (n-2) + (n-3) + ... + 1 + 0 = [(n-1) + 1]/2 * (n - 1) = n(n - 1)/2 = Theta(n^2)
+// For the printlb loop, k is up to the duplicates elements in the array, and we can use it as Theta(n)
+// So, Theta(n^2) + Theta(n) = Theta(n^2)
+//
 
 // (b)
-// Your answer:
+// Your answer: Theta(n^2logn)
 // Justification:
+// For the outer loop, it is Theta ((n^2 - 1)/2)
+// For the inner loop, it is
+// log( n^2/1) + log (n^2/2) + ... + log(n^2/n^2)
+// = log(n^2) - log 1 + log (n^2) -log 2 + ...
+// = n^2log(n^2) - (log 1 + log 2 + ... + log (n^2))
+// = 2n^2logn - (log 1 + log 2 + ... + log (n^2))
+// = Theta(n^2logn) - n^2log(n^2)
+// = Theta(n^2logn)
 
 // You may submit the answer to this question either via Github or
 // write down your solution on paper, take a photo at the end of the exam and upload to Canvas.
@@ -18,9 +31,9 @@ public class Problem1_AlgorithmAnalysis {
         int n = arr.length;
         int[] dup = new int[arr.length];
         int k = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // Theta( n )
             boolean flag = false;
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {// (n-1) + (n - 2) + (n - 3) + ... + 0 = Theta (n^2)
                 if (arr[i] == arr[j]) {
                     flag = true;
                     break;
@@ -31,7 +44,7 @@ public class Problem1_AlgorithmAnalysis {
                 k++;
             }
         }
-        for (int m = 0; m < k; m++) {
+        for (int m = 0; m < k; m++) { // Theta (n)
             System.out.println(dup[m]);
         }
     }
@@ -43,7 +56,7 @@ public class Problem1_AlgorithmAnalysis {
         int i = 1;
         while (i <= n*n)  { // note that i goes to n^2
             int j = 1;
-            while (j <= n*n) { // note that j goes to n^2
+            while (j <= n*n) { // note that j goes to n^2   log (n^2/1) + log
                 sum +=  i + j*j;
                 j = j * 4; // note that j is multiplied by 4
             }
